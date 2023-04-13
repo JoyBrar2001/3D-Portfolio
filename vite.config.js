2cd5eb5601d2c87e71c61781a88dd4b0f222ad80
Joy Brar <joybrar20001@gmail.com>
2023-04-13 16:57:31 +0530

    Final

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/3D-Portfolio/',
+  base: '/3D-Portfolio-Final/',
   plugins: [react()],
 })
